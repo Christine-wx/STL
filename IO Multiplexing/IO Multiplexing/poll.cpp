e48f22149090b83a20d3a4011f395b5af5b1d4a6
wx <1927451510@qq.com>
2024-03-16 13:52:39 +0800

    Rename poll.cpp to poll_API.cpp

--- IO Multiplexing/IO Multiplexing/poll.cpp
+++
@@ -1,7 +0,0 @@
-#include <poll.h>
-int poll(struct pollfd fds[], nfds_t nfds, int timeout)；
-typedef struct pollfd {
-	int fd;                         /* 需要被检测或选择的文件描述符*/
-	short events;                   /* 对文件描述符fd上感兴趣的事件 */
-	short revents;                  /* 文件描述符fd上当前实际发生的事件*/
-} pollfd_t;
